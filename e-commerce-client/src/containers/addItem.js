b74ce2f96b2fc9517ce15907020fd5f712b43170
TaufikAlex <Taufik_Alex@Yahoo.com>
2019-10-25 00:07:37 +0700

    added folder containers, components

---
+++ e-commerce-client/src/containers/addItem.js
@@ -0,0 +1,22 @@
+import React , { Component } from "react";
+import { connect } from "react-redux";
+
+class AddItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title = "",
+            category = "",
+            price = "",
+            rate = "",
+            description = "",
+            detail = "",
+            brand = "",
+            color = [],
+            stock = "",
+            capacity = [],
+            vote = "",
+
+        }
+    }
+}
