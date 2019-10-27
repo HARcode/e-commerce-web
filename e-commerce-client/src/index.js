f709552ceeed2e8e84aafc3dd99eedc20cfc652f
Rofi Syahrul <srofi44@gmail.com>
2019-10-27 22:30:17 +0700

    Tried redux in index.js

--- e-commerce-client/src/index.js
+++ e-commerce-client/src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddItem from '../src/containers/addItem';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import AddItem from "../src/containers/addItem";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import rootReducers from "./reducers";
 
-ReactDOM.render(<AddItem />, document.getElementById('root'));
+let store = createStore(rootReducers);
+console.log(store.getState());
+
+ReactDOM.render(<AddItem />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
